51d45a73b8209a972de6819ab008bfb8509eb79e
razvan <razvan.deac@gmail.com>
2017-10-27 13:37:03 +0300

    setup

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var webpack = require("webpack");
+
+module.exports = {
+	entry: "./src/index.js",
+	output: {
+		path: __dirname + "/public",
+		filename: "bundle.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				loader: "babel-loader",
+				query: {
+					presets: ["env", "react"]
+				}
+			}
+		]
+	}
+}
+
+
+
+
+
+
+
